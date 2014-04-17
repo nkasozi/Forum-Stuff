15706a63f7e55482988c1144edee99b4ee9a32f4
nkasozi <nkasozi@gmail.com>
2014-04-17 14:08:35 +0300

    Kadic Talks v.2

--- KadicTalks/laravel/app/views/layouts/posts.blade.php
+++ KadicTalks/laravel/app/views/layouts/posts.blade.php
@@ -31,8 +31,9 @@
         <img src="images/{{$users[$i]->name_of_pic===''?'guest.png':$users[$i]->name_of_pic}}"/>
       </div>
 
-      <!--header of the post -->
+
       <div class="profile_comment">
+        <!--header of the post -->
         <div class="profile_comment_header">
           <ul>
             <li><a href="profile?id={{$users[$i]->id}}">{{$users[$i]->username}}</a></li>
@@ -47,23 +48,34 @@
         </div>
 
         <!--footer of post -->
-        @if((($posts[$i]->name_of_attachment)!='')||(($posts[$i]->link_to_video)!='')||(Auth::check()))
+        @if((($posts[$i]->name_of_attachment)!='')||(($posts[$i]->link_to_video)!='')||(Auth::check()&&(($users[$i]->id)==(Auth::user()->id))||((Auth::user()->account_type)==='Administrator')))
+
         <div class="profile_comment_attachment">     
           <ul>  
+
             @if(($posts[$i]->name_of_attachment)!='')
             <li><a href="attachments/{{$posts[$i]->name_of_attachment}}" target="_blank"><img src="images/attachment.png" /></a></li>
             @endif
+
             @if(($posts[$i]->link_to_video)!='')
             <li><a href="{{$posts[$i]->link_to_video}}" target="_blank"><img src="images/video.png"/></a></li>   
             @endif
+
             @if(Auth::check())
-            @if(($users[$i]->id)==(Auth::user()->id))
-            <li><a href="edit?id={{$posts[$i]->id}}"><img src="images/edit.png"/></a></li>   
+            
+            @if(($users[$i]->id)==(Auth::user()->id)||((Auth::user()->account_type)==='Administrator'))
             <li><a href="delete_post?id={{$posts[$i]->id}}" onclick="return confirm('Do You Really Want To Delete This Post')">&nbsp;&nbsp;<img src="images/delete.png"></a></li>   
             @endif
+
+            @if(($users[$i]->id)==(Auth::user()->id))
+            <li><a href="edit?id={{$posts[$i]->id}}"><img src="images/edit.png"/></a></li>     
             @endif
+
+            @endif
+
           </ul>
         </div>
+
         @endif
 
       </div>
@@ -72,6 +84,7 @@
   <!-- END OF POST-->
 
   @endfor
+
   @else
   <div class="flash_error" >
     <p>Sorry But There Are no posts to display</p>
@@ -104,14 +117,14 @@
 
         <?php
         $is_user_attachment_allowed_setting = Setting::where('name', '=', 'user_attachment')->first();
-        $user_attachment_allowed            = ($is_user_attachment_allowed_setting->value)==='Yes' ? TRUE : FALSE;
+        $user_attachment_allowed            = ($is_user_attachment_allowed_setting->value) === 'Yes' ? TRUE : FALSE;
         ?>
         <div class="post_comment_footer">
 
           <ul>
             @if((Auth::user()->account_type)==='Administrator'||$user_attachment_allowed)
             <p style="margin-top: 1em;">{{Form::file('attachment')}}<p>
-             @endif
+              @endif
               <!-- submit button -->
             <li> {{ Form::submit('Post',array('id'=>'submit')) }}</li>
           </ul>
